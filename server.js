3fc15c0d97370656e556892dc512d2fc2f3c9b8a
Raju Sen <rajugbp2006@gmail.com>
2026-05-18 22:35:30 +0530

    backend complete

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+app.get("/", (req, res) => {
+  res.send("Lab Backend Running...");
+});
+
 app.post("/api/book-test", async (req, res) => {
   try {
     const { name, mobile, city, test, collectionType } = req.body;
@@ -152,6 +158,8 @@ app.post("/api/book-test", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server Running On Port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Running On Port ${PORT}`);
 });
