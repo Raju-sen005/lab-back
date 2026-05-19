06576ecb66e81cd2a72eb186c841c25894a6c830
Raju Sen <rajugbp2006@gmail.com>
2026-05-19 10:21:23 +0530

    smtp update

--- server.js
+++ server.js
@@ -27,11 +27,17 @@ app.post("/api/book-test", async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
+      family: 4,
+
       auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD,
+      },
+
+      tls: {
+        rejectUnauthorized: false,
       },
     });
 
